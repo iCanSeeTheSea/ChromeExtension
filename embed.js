baf3b7096f36fa095d862dcbdfc31b03e4ff7692
sea <c.eres5519@gmail.com>
2023-03-02 10:09:01 +0000

    initial file upoad

---
+++ embed.js
@@ -0,0 +1,7 @@
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+	let videoId = url.slice(-11)
+	console.log(videoId)
+	chrome.tabs.update({url: 'http://youtube.com/embed/' + videoId});
+	// use `url` here inside the callback because it's asynchronous!
+});
