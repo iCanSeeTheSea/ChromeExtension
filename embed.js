9668238d142cd12a37355b0ed2df0b1508e24d5d
sea <c.eres5519@gmail.com>
2023-03-02 10:43:21 +0000

    use https

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
-	let videoId = url.slice(-11)
-	chrome.tabs.update({url: 'http://youtube.com/embed/' + videoId});
+	let videoId = url.slice(32, 43)
+	chrome.tabs.update({url: 'https://youtube.com/embed/' + videoId});
 	// use `url` here inside the callback because it's asynchronous!
 });
