3d372a0636c7938f00ab74fea2d0556c6dd67715
sea <c.eres5519@gmail.com>
2023-03-06 12:16:29 +0000

    added check for being on youtube.com

--- embed.js
+++ embed.js
@@ -1,6 +1,7 @@
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
-	let videoId = url.slice(32, 43)
-	chrome.tabs.update({url: 'https://youtube.com/embed/' + videoId});
-	// use `url` here inside the callback because it's asynchronous!
+	if (url.includes('https://www.youtube.com/watch?v=')){
+		let videoId = url.slice(32, 43)
+		chrome.tabs.update({url: 'https://youtube.com/embed/' + videoId});
+	}
 });
